e7398591bccc39a2af8d4bbfbdeefb7ad13f32a3
ehmatthes <ehmatthes@gmail.com>
2013-04-02 07:24:26 -0800

    Learning targets are inline with essential understandings on admin site.

--- competencies/admin.py
+++ competencies/admin.py
@@ -8,6 +8,7 @@ class SubdisciplineAreaInline(admin.TabularInline):
 class SubjectAreaAdmin(admin.ModelAdmin):
     inlines = [SubdisciplineAreaInline]
 
+
 class EssentialUnderstandingInline(admin.TabularInline):
     model = EssentialUnderstanding
     extra = 1
@@ -16,6 +17,13 @@ class CompetencyAreaAdmin(admin.ModelAdmin):
     inlines = [EssentialUnderstandingInline]
 
 
+class LearningTargetInline(admin.TabularInline):
+    model = LearningTarget
+    extra = 1
+
+class EssentialUnderstandingAdmin(admin.ModelAdmin):
+    inlines = [LearningTargetInline]
 
 admin.site.register(SubjectArea, SubjectAreaAdmin)
 admin.site.register(CompetencyArea, CompetencyAreaAdmin)
+admin.site.register(EssentialUnderstanding, EssentialUnderstandingAdmin)
