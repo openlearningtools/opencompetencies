7a2f61a1660a67a5c672a71b7a270c5449a8a626
ehmatthes <ehmatthes@gmail.com>
2013-04-04 08:10:25 -0800

    Index page works.

--- competencies/views.py
+++ competencies/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from competencies.models import SubjectArea, SubdisciplineArea, CompetencyArea, EssentialUnderstanding, LearningTarget
+from competencies.models import School, SubjectArea, SubdisciplineArea, CompetencyArea, EssentialUnderstanding, LearningTarget
 
 
 def index(request):
     return render_to_response('competencies/index.html')
+
+def schools(request):
+    schools = School.objects.all()
+    return render_to_response('competencies/schools.html',{'schools': schools})
 
 def entire_system(request):
     subject_areas = SubjectArea.objects.all()
