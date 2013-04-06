0db27e07c61deae3e81f3f6104df293016f69786
ehmatthes <ehmatthes@gmail.com>
2013-04-06 06:32:36 -0800

    Test suite has create_school function, and all tests pass including school detail view.

--- competencies/tests.py
+++ competencies/tests.py
@@ -3,6 +3,11 @@ from django.core.urlresolvers import reverse
 
 from competencies.models import *
 
+
+def create_school(name):
+    """Creates a school with the given name."""
+    return School.objects.create(name=name)
+
 class CompetencyViewTests(TestCase):
     def test_index_view(self):
         """Index page is a static page for now, so just check status."""
@@ -16,6 +21,6 @@ class CompetencyViewTests(TestCase):
 
     def test_school_view(self):
         """School page lists subject areas and subdiscipline areas for that school."""
-        #response = self.client.get(reverse('competencies:schools' 1))
-        #self.assertEqual(response.status_code, 200)
-        pass
+        django_school = create_school(name="The Django School")
+        response = self.client.get(reverse('competencies:school', args=(django_school.id,)))
+        self.assertEqual(response.status_code, 200)
