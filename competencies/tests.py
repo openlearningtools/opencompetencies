d83e8716341b2beb805a070c6513ee172948db3f
ehmatthes <ehmatthes@gmail.com>
2013-04-07 23:34:31 -0800

    Simple test for subject_area view works.

--- competencies/tests.py
+++ competencies/tests.py
@@ -8,7 +8,16 @@ def create_school(name):
     """Creates a school with the given name."""
     return School.objects.create(name=name)
 
+def create_subject_area(subject_area, school):
+    """Creates a subject_area, with fk to given school."""
+    return SubjectArea.objects.create(subject_area=subject_area, school=school)
+
 class CompetencyViewTests(TestCase):
+    """Needed tests:
+    - queryset tests for all pages
+    - no-data tests
+    """
+
     def test_index_view(self):
         """Index page is a static page for now, so just check status."""
         response = self.client.get(reverse('competencies:index'))
@@ -24,3 +33,10 @@ class CompetencyViewTests(TestCase):
         django_school = create_school(name="The Django School")
         response = self.client.get(reverse('competencies:school', args=(django_school.id,)))
         self.assertEqual(response.status_code, 200)
+
+    def test_subject_area_view(self):
+        """Subject area page lists general competencies for a single subject area,
+        and competencies for that subject's subdiscipline areas."""
+        django_school = create_school(name="The Django School")
+        science = create_subject_area(subject_area="Science", school=django_school)
+        response = self.client.get(reverse('competencies:subject_area', args=(django_school.id, science.id,)))
