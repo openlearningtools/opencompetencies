037415c4e4112384540acbb821220a7ab4b53092
ehmatthes <ehmatthes@gmail.com>
2013-04-16 21:57:23 -0800

    Local one-line test project-wide css file works.

--- opencompetencies/settings.py
+++ opencompetencies/settings.py
@@ -65,6 +65,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/srv/projects/oc_dev/opencompetencies/opencompetencies/static/',
+    '/srv/projects/oc_dev/opencompetencies/competencies/static',
 )
 
 # List of finder classes that know how to find static files in
