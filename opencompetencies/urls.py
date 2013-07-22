7af85b58f93516f583b239e6958d0661111862be
Eric Matthes <ehmatthes@gmail.com>
2013-07-22 07:42:18 -0800

    Clarified why leaving heroku static setup in urls.py for now.

--- opencompetencies/urls.py
+++ opencompetencies/urls.py
@@ -28,8 +28,10 @@ urlpatterns = patterns('',
 
 
 # Set up static files appropriate to environment
-#  Right now, all static files in one place, not using collectstatic
+#  Right now, all static files in one place (except admin), not using collectstatic
+#  Should probably switch to dj-static at some point shortly
 if os.environ.get('DEPLOY_ENVIRONMENT', None) == 'heroku':
+    # Now using rackspace, but keeping this for anyone who wants to set up a dev site on heroku
     # heroku static files setup:
     urlpatterns += patterns('',
                             (r'^static/(.*)$', 'django.views.static.serve',
