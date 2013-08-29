83a6ac037d968a67b9eb1ff0477adb3d69dd6f6f
Eric Matthes <ehmatthes@gmail.com>
2013-08-29 06:36:27 -0800

    Scraper saves new components, but no prior existing check.

--- django_ccss/ccss_import_via_scrape.py
+++ django_ccss/ccss_import_via_scrape.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
 import requests
+from django_ccss.models import *
 
-def get_statement_from_url(url):
+standard = Standard.objects.get(pk=2)
+
+def get_statement_from_url(url, component_dn):
     r = requests.get(url)
     if r.status_code == 200:
 
@@ -17,6 +20,12 @@ def get_statement_from_url(url):
                 return False
         component = root.findall('.//Statement')[0]
         print '\nComponent:', component.text
+        new_component = Component()
+        new_component.component = component.text
+        new_component.standard = standard
+        new_component.dot_notation = 'CCSS.ELA-Literacy.W.9-10.2.' + component_dn
+        new_component.save()
+        print '\n*** nc: ', new_component
         return True
     else:
         print 'failed', r.status_code, type(r.status_code)
@@ -25,14 +34,8 @@ def get_statement_from_url(url):
 
 for component_dn in ['a','b','c','d','e','f','g','h','i']:
     urlstring = 'http://www.corestandards.org/ELA-Literacy/W/9-10/2/' + component_dn + '.xml'
-    element_found = get_statement_from_url(urlstring)
+    element_found = get_statement_from_url(urlstring, component_dn)
     if not element_found:
         break
-
-from django_ccss.models import *
-standards = Standard.objects.all()
-for standard in standards:
-    print '\n\npk, standard:', standard.pk, standard
-
 
 print '\n\nFinished.\n\n'
