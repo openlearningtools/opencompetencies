f54eb4380af132be279394fa1d87d862cfa499dd
Eric Matthes <ehmatthes@gmail.com>
2014-04-03 15:55:37 -0800

    Started test_fork_school.

--- competencies/tests/test_fork_schools.py
+++ competencies/tests/test_fork_schools.py
@@ -1,7 +1,19 @@
 from django.test import TestCase
-from django.core.urlresolvers import reverse
 
 from competencies.models import *
+import testing_utilities as tu
 
+class TestForkSchools(TestCase):
 
+    def setUp(self):
+        # Create a school.
+        self.school_0 = tu.create_school(name="School 0")
 
+    def test_fork_school(self):
+        # Make a new school, and fork school_o's system.
+        pass
+
+    def test_fork_school_from_view(self):
+        # Do the same thing as test_fork_school, but through 
+        #  view interface.
+        pass
