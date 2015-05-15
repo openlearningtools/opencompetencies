eea8bb55d26f2e2403c1a82233aac02e16df24eb
Eric Matthes <ehmatthes@gmail.com>
2015-05-15 10:54:15 -0800

    Minimum changes necessary to get the project running on Django 1.8.

--- competencies/models.py
+++ competencies/models.py
@@ -230,57 +230,57 @@ class LearningTargetForm(ModelForm):
                    'description': Textarea(attrs={'rows': 5, 'class': 'span8'}),
                    }
 
-class PathwayForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(PathwayForm, self).__init__(*args, **kwargs)
-        pathway = self.instance
+# class PathwayForm(ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super(PathwayForm, self).__init__(*args, **kwargs)
+#         pathway = self.instance
 
-        # sas
-        self.fields['subject_areas'].queryset = pathway.school.subjectarea_set.all()
+    #     # sas
+    #     self.fields['subject_areas'].queryset = pathway.school.subjectarea_set.all()
 
-        # sdas
-        if pathway.subject_areas.all():
-            sda_queryset = SubdisciplineArea.objects.none()
-            for sa in pathway.subject_areas.all():
-                sda_queryset = sda_queryset | sa.subdisciplinearea_set.all()
-            self.fields['subdiscipline_areas'].queryset = sda_queryset
+    #     # sdas
+    #     if pathway.subject_areas.all():
+    #         sda_queryset = SubdisciplineArea.objects.none()
+    #         for sa in pathway.subject_areas.all():
+    #             sda_queryset = sda_queryset | sa.subdisciplinearea_set.all()
+    #         self.fields['subdiscipline_areas'].queryset = sda_queryset
 
-        # cas
-        if pathway.subject_areas.all():
-            ca_queryset = CompetencyArea.objects.none()
-            # General subject area competencies:
-            for sa in pathway.subject_areas.all():
-                ca_queryset = ca_queryset | sa.competencyarea_set.all().filter(subdiscipline_area=None)
-            # Subdiscipline area competencies:
-            if pathway.subdiscipline_areas.all():
-                for sda in pathway.subdiscipline_areas.all():
-                    ca_queryset = ca_queryset | sda.competencyarea_set.all()
-            self.fields['competency_areas'].queryset = ca_queryset
+    #     # cas
+    #     if pathway.subject_areas.all():
+    #         ca_queryset = CompetencyArea.objects.none()
+    #         # General subject area competencies:
+    #         for sa in pathway.subject_areas.all():
+    #             ca_queryset = ca_queryset | sa.competencyarea_set.all().filter(subdiscipline_area=None)
+    #         # Subdiscipline area competencies:
+    #         if pathway.subdiscipline_areas.all():
+    #             for sda in pathway.subdiscipline_areas.all():
+    #                 ca_queryset = ca_queryset | sda.competencyarea_set.all()
+    #         self.fields['competency_areas'].queryset = ca_queryset
 
-        # eus
-        if pathway.competency_areas.all():
-            eu_queryset = EssentialUnderstanding.objects.none()
-            for ca in pathway.competency_areas.all():
-                eu_queryset |= ca.essentialunderstanding_set.all()
-            self.fields['essential_understandings'].queryset = eu_queryset
+    #     # eus
+    #     if pathway.competency_areas.all():
+    #         eu_queryset = EssentialUnderstanding.objects.none()
+    #         for ca in pathway.competency_areas.all():
+    #             eu_queryset |= ca.essentialunderstanding_set.all()
+    #         self.fields['essential_understandings'].queryset = eu_queryset
 
-        # lts
-        if pathway.essential_understandings.all():
-            lt_queryset = LearningTarget.objects.none()
-            for eu in pathway.essential_understandings.all():
-                lt_queryset |= eu.learningtarget_set.all()
-            self.fields['learning_targets'].queryset = lt_queryset
+    #     # lts
+    #     if pathway.essential_understandings.all():
+    #         lt_queryset = LearningTarget.objects.none()
+    #         for eu in pathway.essential_understandings.all():
+    #             lt_queryset |= eu.learningtarget_set.all()
+    #         self.fields['learning_targets'].queryset = lt_queryset
 
-    class Meta:
-        model = Pathway
-        # Bootstrap controls width of Textarea, ignoring the 'cols' setting. Can also use 'class': 'input-block-level'
-        #  Consider CheckboxSelectMultiple for some of these, especially eus and lts
-        # number of items to show in longer dropdown lists:
-        widgets = {
-            'name': TextInput(attrs={'class': 'span4'}),
-            'subject_areas': SelectMultiple(attrs={'class': 'span4', 'size': 5}),
-            'subdiscipline_areas': SelectMultiple(attrs={'class': 'span4', 'size': 5}),
-            'competency_areas': SelectMultiple(attrs={'class': 'span4', 'size': 15}),
-            'essential_understandings': SelectMultiple(attrs={'class': 'span8', 'size': 20}),
-            'learning_targets': SelectMultiple(attrs={'class': 'span8', 'size': 20}),
-            }
+    # class Meta:
+    #     model = Pathway
+    #     # Bootstrap controls width of Textarea, ignoring the 'cols' setting. Can also use 'class': 'input-block-level'
+    #     #  Consider CheckboxSelectMultiple for some of these, especially eus and lts
+    #     # number of items to show in longer dropdown lists:
+    #     widgets = {
+    #         'name': TextInput(attrs={'class': 'span4'}),
+    #         'subject_areas': SelectMultiple(attrs={'class': 'span4', 'size': 5}),
+    #         'subdiscipline_areas': SelectMultiple(attrs={'class': 'span4', 'size': 5}),
+    #         'competency_areas': SelectMultiple(attrs={'class': 'span4', 'size': 15}),
+    #         'essential_understandings': SelectMultiple(attrs={'class': 'span8', 'size': 20}),
+    #         'learning_targets': SelectMultiple(attrs={'class': 'span8', 'size': 20}),
+    #         }
