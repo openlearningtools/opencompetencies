5641ed175f269793c98b3e3a56dc22fdb3a601ef
Eric Matthes <ehmatthes@gmail.com>
2015-06-19 23:06:58 -0800

    Testing starting to work for logged in user.

--- competencies/tests/tests.py
+++ competencies/tests/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 from competencies.models import *
+from competencies.views import school
 
 
 class CompetencyViewTests(TestCase):
@@ -12,8 +14,29 @@ class CompetencyViewTests(TestCase):
     """
 
     def setUp(self):
+        # Build a school, down to the performance indicator level.
+        #  These should be a few loops, making a number of elements at each level.
+
+        self.client = Client()
+
+        # Create a test user.
+        self.test_user = User.objects.create(username='testuser')
+        self.test_user.set_password('pw')
+        self.test_user.save()
+        new_up = UserProfile()
+        new_up.user = self.test_user
+        new_up.save()
+
+        # Create test school.
         self.test_school = School.objects.create(name="Test School")
         self.test_sa = SubjectArea.objects.create(subject_area="Science", school=self.test_school)
+        self.test_sda = SubdisciplineArea.objects.create(subject_area=self.test_sa,
+                                                         subdiscipline_area="Physical Science")
+
+        # Give test user permissions to work with this school.
+        self.test_user.userprofile.schools.add(self.test_school)
+
+
 
     def test_index_view(self):
         """Index page is a static page for now, so just check status."""
@@ -31,8 +54,35 @@ class CompetencyViewTests(TestCase):
 
     def test_school_view(self):
         """School page lists subject areas and subdiscipline areas for that school."""
+        logged_in = self.client.login(username='testuser', password='pw')
+        print('logged in: ', logged_in)
+        
         response = self.client.get(reverse('competencies:school', args=(self.test_school.id,)))
         self.assertEqual(response.status_code, 200)
+
+        # Make sure subject and sda appear in context.
+        self.assertTrue('school' in response.context)
+        self.assertTrue('subject_areas' in response.context)
+        self.assertTrue('sa_sdas' in response.context)
+
+        #return 'done'
+        # Is this because I'm not logged in?
+        # Yes! Create a user and log in?
+        #  Test logged in, logged out version of page.
+        print(response.context['user'])
+        print(response.context['school'])
+        print(response.context['subject_areas'])
+        print(response.context['sa_sdas'])
+        print(response.context['sa_sdas'].keys())
+        print(response.context.keys())
+        print(reverse('competencies:school', args=(self.test_school.id,)))
+        
+        return 'done'
+
+
+        self.assertEqual(self.test_school, response.context['school'])
+        self.assertTrue(self.test_sa in response.context['subject_areas'])
+        #self.assertTrue(self.test_sda in response.context['sa_sdas'][self.test_sa])
 
     def test_new_school(self):
         """New school processes form to create a new school.
