5b88bdcdfefd4a294e54c05b58a9a3e3848ae89f
Eric Matthes <ehmatthes@gmail.com>
2015-07-28 22:29:12 -0800

    Added public attribute to organization.

--- competencies/models.py
+++ competencies/models.py
@@ -17,6 +17,7 @@ class Organization(models.Model):
     name = models.CharField(max_length=500)
     org_type = models.CharField(max_length=500, default='school')
     owner = models.ForeignKey(User)
+    public = models.BooleanField(default=False)
 
     # Allow organizations to rename taxonomy elements.
     alias_sa = models.CharField(max_length=500, default='subject area')
